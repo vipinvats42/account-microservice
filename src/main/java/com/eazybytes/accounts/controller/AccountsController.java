4b9cbc9fcf2dbc7c968b16f447c21e96b84ff6fd
Vipin <=vipinvats42@gmail.com>
2025-05-24 08:11:27 +0530

    reading config using environment variable

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,6 +1,8 @@
 package com.eazybytes.accounts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +40,9 @@ public class AccountsController {
 
         @Value("${build.version}")
         private String buildVersion;
+
+        @Autowired
+        private Environment environment;
 
         public AccountsController(IAccountService iAccountService) {
                 this.iAccountService = iAccountService;
@@ -154,6 +159,19 @@ public class AccountsController {
                 return ResponseEntity
                                 .status(HttpStatus.OK)
                                 .body(new ResponseDTO(AccountsConstants.status_200, buildVersion));
+        }
+
+        @Operation(summary = "Get Java version", description = "Get Java version installed into acccount microservice")
+        @ApiResponses({
+                        @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+                        @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+        })
+
+        @GetMapping("/java-version")
+        ResponseEntity<String> getJavaVersion() {
+                return ResponseEntity
+                                .status(HttpStatus.OK)
+                                .body(environment.getProperty("JAVA_HOME"));
         }
 
 }
