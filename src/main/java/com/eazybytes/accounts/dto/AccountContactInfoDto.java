0513667fda42f59ca57f5ab1b68f6f4bd25ab257
Vipin <=vipinvats42@gmail.com>
2025-06-06 04:25:04 +0530

    refresh config at runtime

--- src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
+++ src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
@@ -5,7 +5,16 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
